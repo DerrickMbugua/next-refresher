1b287df1dc085733bbfac7e400a5aba96f7c9c81
“derrick” <derrickmbugua50@gmail.com>
2023-10-10 07:32:12 +0300

    feat: small project

--- next01/app/users/[userId]/page.tsx
+++ next01/app/users/[userId]/page.tsx
@@ -1,5 +1,6 @@
 import getUser from "@/lib/getUser"
 import getUserPosts from "@/lib/getUserPosts"
+import getAllUsers from "@/lib/getAllUsers"
 import { Suspense } from "react"
 import UserPosts from "./components/UserPosts"
 import { Metadata } from 'next'
@@ -31,4 +32,13 @@ export default async function UserPage({ params }: { params: { userId: string } 
 
     </div>
   )
+}
+
+export async function generateStaticParams() {
+ const usersData: Promise<User[]> = getAllUsers()
+ const users = await usersData
+
+ return users.map(user => ({
+  userId: user.id.toString()
+ }))
 }
